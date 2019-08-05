cb9f1d7e333f1337f8ab07177efdda51bdf89f4e
Greg Zahora <gregzahora3@gmail.com>
2019-08-05 19:51:36 -0400

    added some style elements

--- FriendFinder/app/data/friends.js
+++ FriendFinder/app/data/friends.js
@@ -1,27 +1,27 @@
 var friends = [
 	{
 		name: "Hiking Greg",
-		photo: "https://scontent-iad3-1.xx.fbcdn.net/v/t1.0-9/29571393_10157523726959062_240812810347546844_n.jpg?_nc_cat=104&_nc_oc=AQkGLZezDaFtK-dCjgdkDOttLiXeDPQ0vy9nGy44HwMkgDE8Fm_0LWWHUSnf55xzPQ-p5lNlOn4gebe66ttfpf1P&_nc_ht=scontent-iad3-1.xx&oh=7e694be947d812a33139ee600803a9d9&oe=5DDD1C8C",
+		photo: "https://scontent-iad3-1.xx.fbcdn.net/v/t1.0-9/21271114_10155433755284718_6512553493599260098_n.jpg?_nc_cat=111&_nc_oc=AQluwYv1YOpks9dN8w_nHinnloiNWNRokdOEtJmMh6_VUKbaq5a7kFNC1neIN5n5GGcyqqD-Eqf58k75HtjsSd2x&_nc_ht=scontent-iad3-1.xx&oh=6d6481a7b7c5e72bfcddcecef44608aa&oe=5DDE29A6",
 		scores: [5, 1, 3, 2, 4, 2, 5, 2, 5, 4]
 	},
 	{
-		name: "Party Greg",
-		photo: "https://scontent-iad3-1.xx.fbcdn.net/v/t1.0-9/29571393_10157523726959062_240812810347546844_n.jpg?_nc_cat=104&_nc_oc=AQkGLZezDaFtK-dCjgdkDOttLiXeDPQ0vy9nGy44HwMkgDE8Fm_0LWWHUSnf55xzPQ-p5lNlOn4gebe66ttfpf1P&_nc_ht=scontent-iad3-1.xx&oh=7e694be947d812a33139ee600803a9d9&oe=5DDD1C8C",
+		name: "Toddler-Triplet Greg",
+		photo: "https://scontent-iad3-1.xx.fbcdn.net/v/t1.0-9/50115480_10157126096546410_5097320109479821312_n.jpg?_nc_cat=105&_nc_oc=AQlMRByH2H9is4hlILmRILAiJmnydpw1TDBp2-5Yl-iVCyJ8AWiwrQNY7BYAjNAeUQ8zZan29DacMvmobSGO5Zm2&_nc_ht=scontent-iad3-1.xx&oh=8eeda4b928376399c2c1334d289be1ed&oe=5DD37035",
 		scores: [2, 5, 5, 2, 2, 5, 5, 4, 1, 5]
 	},
 	{
-		name: "Studious Greg",
-		photo: "https://scontent-iad3-1.xx.fbcdn.net/v/t1.0-9/29571393_10157523726959062_240812810347546844_n.jpg?_nc_cat=104&_nc_oc=AQkGLZezDaFtK-dCjgdkDOttLiXeDPQ0vy9nGy44HwMkgDE8Fm_0LWWHUSnf55xzPQ-p5lNlOn4gebe66ttfpf1P&_nc_ht=scontent-iad3-1.xx&oh=7e694be947d812a33139ee600803a9d9&oe=5DDD1C8C",
+		name: "Cold Greg",
+		photo: "https://scontent-iad3-1.xx.fbcdn.net/v/t1.0-9/46515927_10156078428728333_402333230411284480_n.jpg?_nc_cat=109&_nc_oc=AQncFK7G4g7Vcq-zHksc3EcPWuD13nqXJyInuCunoT4k_0iBcSD5kUyQWQYmBZp1fv8I_0Ys3DQ3OLPoS6SstYsP&_nc_ht=scontent-iad3-1.xx&oh=4e2e51b0ad52f3b273b792a23ca90b9f&oe=5DE6AA93",
 		scores: [2, 3, 2, 5, 2, 5, 1, 3, 1, 4]
 	},
 	{
 		name: "Chill Greg",
-		photo: "https://scontent-iad3-1.xx.fbcdn.net/v/t1.0-9/29571393_10157523726959062_240812810347546844_n.jpg?_nc_cat=104&_nc_oc=AQkGLZezDaFtK-dCjgdkDOttLiXeDPQ0vy9nGy44HwMkgDE8Fm_0LWWHUSnf55xzPQ-p5lNlOn4gebe66ttfpf1P&_nc_ht=scontent-iad3-1.xx&oh=7e694be947d812a33139ee600803a9d9&oe=5DDD1C8C",
+		photo: "https://scontent-iad3-1.xx.fbcdn.net/v/t1.0-9/68458653_10157128049899718_7534719852460638208_n.jpg?_nc_cat=108&_nc_oc=AQnmiejCjodnstLMxLFeCk7yLMUIZt1QTPNN70GHJvAXC7rSDJ7THAhYRIENLxn8Smiko2CZUCB0_0EhkD2ARLVS&_nc_ht=scontent-iad3-1.xx&oh=ab1ec60732923385dddee8621cee2718&oe=5DE0DE48",
 		scores: [3, 3, 3, 3, 3, 3, 3, 3, 3, 5]
 	},
 	{
-		name: "Sleepy Greg",
-		photo: "https://scontent-iad3-1.xx.fbcdn.net/v/t1.0-9/29571393_10157523726959062_240812810347546844_n.jpg?_nc_cat=104&_nc_oc=AQkGLZezDaFtK-dCjgdkDOttLiXeDPQ0vy9nGy44HwMkgDE8Fm_0LWWHUSnf55xzPQ-p5lNlOn4gebe66ttfpf1P&_nc_ht=scontent-iad3-1.xx&oh=7e694be947d812a33139ee600803a9d9&oe=5DDD1C8C",
+		name: "Wedding Season Greg",
+		photo: "https://scontent-iad3-1.xx.fbcdn.net/v/t31.0-8/23668888_10155973685702425_3840922817268527889_o.jpg?_nc_cat=106&_nc_oc=AQnOM6li_XxdMvZpYYpt-Ug9dLtHtiegTZ_1ZQdsvB91KvXawkiuUqh2XJAiXQyKVITE5DOamgSRdTvfqPLLBawA&_nc_ht=scontent-iad3-1.xx&oh=9f4419e0170ce514827fbbbbdd6ae9e0&oe=5DDA3831",
 		scores: [1, 1, 2, 1, 1, 2, 2, 2, 5, 3]
 	}
 ];
